cb71f3cf66b41ef984740bbbfeb5f31be8c71611
acelaena <62402838+acelaena@users.noreply.github.com>
2021-02-04 02:21:29 -0800

    simpleshapes

---
+++ SimpleShapes/sketch.js
@@ -0,0 +1,15 @@
+
+// Setup code goes here
+function setup() {
+  createCanvas(600, 400);
+    textSize(20);
+    textAlign(CENTER);
+ }
+
+
+// Draw code goes here
+function draw() {
+  background(0);
+    fill('#cf48a6');
+    text("Hello World", width/2, height/2);
+}

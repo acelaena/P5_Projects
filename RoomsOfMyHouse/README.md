79b4d56097b40c82e6dec98df1dc73ad88067170
acelaena <62402838+acelaena@users.noreply.github.com>
2021-03-23 20:27:18 -0700

    bleh

--- RoomsOfMyHouse/README.md
+++ RoomsOfMyHouse/README.md
@@ -1,19 +1,17 @@
-## Simple State Machine
-#### by Scott Kildall
+## Rooms of my House
+#### by Alina Xia
 
 
 ### Overview
-This is a template for a simple state machine that goes between five states
+A simple project using a map-like data structure to navigate through nodes, or rooms, of a house. Each node represents one state of the state machine. 
 
+### Notes
+- Rooms.js is the main data structure used in this project. 
+- Kitchen image source: https://www.extraspace.com/blog/home-organization/room-organization/design-a-minimalist-kitchen-with-these-ideas/
+- Bathroom image source: http://www.home-designing.com/modern-minimalist-bathroom-interior-design-decor-photos
+- office image source: https://www.spacejoy.com/interior-designs-blog/7-home-office-setups-to-get-you-inspired
 
-### Modifying the Template
+- I (very obviously) burnt out along doing the background artwork, and later backgrounds became more of photo edits rather than photo paintovers. 
+- Mug design by LookHUMAN
+- All other uncredited art by me
 
-(1) Add your own PNG files in the assets folder. Make sure they match the names ***exactly*** of the existing PNGs.
-
-(2) Add custom drawing code to drawOne(), drawTwo(), drawThree(), drawFour(), drawFive()
-
-(3) You can add your own interfaces - keys, mouse events, etc in the Interfaces section
-
-Don't change anything else! 
-
-### How it works
--- RoomsOfMyHouse/sketch.js
+++ RoomsOfMyHouse/sketch.js
@@ -7,47 +7,49 @@
 
 	Notes:
     - Kitchen image source: https://www.extraspace.com/blog/home-organization/room-organization/design-a-minimalist-kitchen-with-these-ideas/
-    - add navigation prompts
-    - add low opacity background box behind text? how to do
+    - Bathroom image source: http://www.home-designing.com/modern-minimalist-bathroom-interior-design-decor-photos
+    - office image source: https://www.spacejoy.com/interior-designs-blog/7-home-office-setups-to-get-you-inspired
     - p5 smelly I could do this faster with html/css/js
 
 */
 var drawFunction;
 var house; 
 var currentRoom;
+var imgSize;
 
 let splash;
 let blueprint;
 let blueprint_mini;
+let key_img = [];
 
 function houseSetup(){
     house = new House(); 
     
-    var greenhouse = new Room("Greenhouse", "An art studio filled with plants, art, and comfy furniture. One wall is filled with ceiling to floor windows,\n and the other is painted with a magical landscape. This probably used to be the living room.", loadImage('https://acelaena.github.io/assets/greenhouse.PNG')); 
+    var greenhouse = new Room("Greenhouse", "An art studio filled with plants, art, and comfy furniture. One wall is filled with ceiling to floor windows,\n and the other is painted with a magical landscape. This probably used to be the living room.", loadImage('assets/greenhouse.PNG')); 
         house.addRoom(greenhouse);
         house.setEntrance(greenhouse);
     
-    var kitchen = new Room("Kitchen", "A bare-minimum appearing kitchen. Everything is carefully stashed.", loadImage('https://acelaena.github.io/assets/kitchen.PNG'));
+    var kitchen = new Room("Kitchen", "A bare-minimum appearing kitchen. Everything is carefully stashed.", loadImage('assets/kitchen.PNG'));
         house.addRoom(kitchen);
         kitchen.setWest(greenhouse);
         greenhouse.setNorth(kitchen);
 
-    var bathroom = new Room("Bathroom", "A minimalist bathroom with just a toilet, sink, and shower.", loadImage('https://acelaena.github.io/assets/bathroom.PNG'));
+    var bathroom = new Room("Bathroom", "A minimalist bathroom with just a toilet, sink, and shower.", loadImage('assets/bathroom.PNG'));
         house.addRoom(bathroom);
         bathroom.setWest(kitchen);
         kitchen.setEast(bathroom);
 
-    var bedroom = new Room("Bedroom", "Compared to the rest of the house, this room looks very lived-in. It's small, barely more than a bed and a closet, but the bookcase and assortment of plushies take up the remaining space nicely.", loadImage('https://acelaena.github.io/assets/bedroom.PNG'));
+    var bedroom = new Room("Bedroom", "Compared to the rest of the house, this room looks very lived-in. It's small, barely more than a bed and a closet, but the assortment of plushies take up the remaining space nicely.", loadImage('assets/bedroom.PNG'));
         house.addRoom(bedroom);
         bedroom.setWest(greenhouse);
         greenhouse.setSouth(bedroom);
     
-    var office = new Room("Office", "Is this really an office..? The walls are covered in art, a whiteboard, and a mirror. On the desk is two oversized monitors, a Wacom Cintiq, and a mess of wires. The L shaped desk extension holds a tea kettle, a half eaten plate of food, and a mess of papers.", loadImage('https://acelaena.github.io/assets/office.PNG'));
+    var office = new Room("Office", "Is this really an office..? The walls are covered in art, a whiteboard, and a mirror. On the desk is two oversized monitors, a Wacom Cintiq, and a mess of wires. The L shaped desk extension holds a tea kettle, a half eaten plate of food, and a mess of papers.", loadImage('assets/office.PNG'));
         house.addRoom(office);
         office.setNorth(bathroom);
         bathroom.setEast(office);
     
-    var hallway = new Room("Hallway", "A corridor with doors on all sides. Were you expecting anything else?", loadImage('https://acelaena.github.io/assets/hallway.PNG'));
+    var hallway = new Room("Hallway", "A corridor with doors on all sides. Were you expecting anything else?", loadImage('assets/hallway.PNG'));
         house.addRoom(hallway);
         kitchen.setSouth(hallway);
         hallway.setNorth(kitchen);
@@ -65,55 +67,76 @@ function houseSetup(){
     Draws an object of type Room to the canvas. 
 */
 function drawRoom(){
-    image(currentRoom.getImg(), 0, 0, width, width*5/9);
-    
-    textAlign(CENTER);
-    textSize(18);
-    fill('#18134d');
-    text("Use WASD directions to navigate the rooms of the house.", width/2, 50);
-    
-    
+    image(currentRoom.getImg(), 0, 0, imgSize, imgSize*5/9);    
 
+    drawText();
+    drawMinimap();
+}
+
+function drawText(){
+    
+    fill('#fff8');
+    rect(0, height-200, width, 200);
+    textAlign(CENTER);
+    fill('#18134d');
     textSize(24);
     textStyle(BOLD);
-    text(currentRoom.getName(), width/2, 550);
+    text(currentRoom.getName(), width/2, height-150);
     
     textSize(16);
     textStyle(ITALIC);
-    text(currentRoom.getInfo(), 0, 570, width-40, 100);
-
-    //drawMinimap();
+    text(currentRoom.getInfo(), 200, height-120, width-400, 100);
 }
 
 /*
     Draws the current minimap. 
 */
 function drawMinimap(){
-    mk = createGraphics(width, height);
-    mk.ellipse(width-50, (width*5/9)-50, 80, 80);
- 
-    blueprint_mini.mask(mk); 
-    image(blueprint_mini, width-50, (width*5/9)-50, 80, 80); 
+    image(blueprint_mini, width-175, height-175, 150, 150); 
+    image(key_img[0], width-114, height-187);
+    image(key_img[1], width-187, height-114);
+    image(key_img[2], width-114, height-44);
+    image(key_img[3], width-44, height-114);
+    
+    
+    //refactor later to put this data in room object
+    if(currentRoom.name == "Greenhouse"){
+        image(key_img[4], width-156, height-144);
+    } else if (currentRoom.name == "Kitchen") {
+        image(key_img[4], width-122, height-144);
+    } else if (currentRoom.name == "Hallway") {
+        image(key_img[4], width-112, height-128);
+    } else if (currentRoom.name == "Bedroom") {
+        image(key_img[4], width-122, height-110);
+    } else if (currentRoom.name == "Office") {
+        image(key_img[4], width-78, height-128);
+    } else if (currentRoom.name == "Bathroom") {
+        image(key_img[4], width-104, height-144);
+    }
 }
 
 /*
     Draws the splash screen. 
 */
 function drawSplash() {
-   image(splash, 0, 0, width, width*5/9);
+   image(splash, 0, 0, imgSize, imgSize*5/9);
+
 }
 
 /*
     Draws the Info screen. 
 */
 function drawInfo(){
-    image(blueprint, 0, 0, width, width*5/9);
-    fill(255);
+    image(blueprint, 0, 0, imgSize, imgSize*5/9);
+    fill('#fff8');
+    rect(0, height-200, width, 200);
+    
+    fill(0);
     textStyle(ITALIC);
     textSize(22);
     var infoText = ["Use WASD directions to navigate the rooms of the house.", "Hit 'X' to return to the splash screen.", "Hit 'I' to return to the map screen.", "Click or press any key again to start house tour."] 
     for(var i = 0; i<infoText.length; i++){
-        text(infoText[i], width/2, height+i*25-80);
+        text(infoText[i], 175, height-150+i*30, width-350, 100);
     }
 }
 
@@ -187,9 +210,16 @@ function mousePressed() {
 
 
 function preload() {
-    splash = loadImage('https://acelaena.github.io/assets/splash.PNG');
-    blueprint = loadImage('https://acelaena.github.io/assets/blueprint.PNG');
-    //blueprint_mini = loadImage('https://acelaena.github.io/assets/blueprint_sm.PNG');
+    splash = loadImage('assets/splash.PNG');
+    blueprint = loadImage('assets/blueprint.PNG');
+    blueprint_mini = loadImage('assets/blueprint_sm.PNG');
+    key_img[0] = loadImage('assets/W.PNG');
+    key_img[1] = loadImage('assets/A.PNG');
+    key_img[2] = loadImage('assets/S.PNG');
+    key_img[3] = loadImage('assets/D.PNG');
+    key_img[4] = loadImage('assets/arrow.PNG');
+
+    
 }
 
 function setup() {
@@ -200,6 +230,11 @@ function setup() {
     textFont('Quicksand');
     textAlign(CENTER);
     textSize(24);
+    
+    imgSize = width;
+    if (height > 5/9*width){
+        imgSize = 9/5*height;
+    }
     
     
     drawFunction = drawSplash;

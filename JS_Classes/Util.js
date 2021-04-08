95041f680eb228b79c0261fced416cb429f3b1fc
acelaena <62402838+acelaena@users.noreply.github.com>
2021-04-08 05:50:19 -0700

    yoot

--- JS_Classes/Util.js
+++ JS_Classes/Util.js
@@ -2,7 +2,8 @@ function rand(min, max) {
   return Math.round(Math.random() * (max-min) + min);
 }
 
-function fontStyle(fsize = 12, align = LEFT, style = NORMAL, lineheight = fsize*1.2, font = null){
+function fontStyle(fsize = 16, color = "#000", align = LEFT, style = NORMAL, lineheight = fsize*1.2, font = null){
+    fill(color);
     textSize(fsize);
     textAlign(align);
     textStyle(style);
@@ -13,6 +14,6 @@ function fontStyle(fsize = 12, align = LEFT, style = NORMAL, lineheight = fsize*
     }
 }
 
-function flip(){
+function flipCoin(){
     return rand (-1, 1) > 0; 
 }
---
+++ VNGameProject/sketch.js
@@ -0,0 +1,80 @@
+/**
+	Project 2 
+	by Acelaena
+
+    Overview:
+
+	Notes:
+
+*/
+const CANVAS_WIDTH = 960;
+const CANVAS_HEIGHT= 600;
+
+var center_x;
+var center_y;
+
+var wendy_sprite = [];
+var textClicker; 
+
+
+var drawAgain = true;
+
+/*
+    Keyboard navigation support. While on a non-room screen, all keys will advance the screen.
+*/
+function keyTyped() {
+    console.log(key);
+}
+
+function wendyExp(expression){
+    image(wendy_sprite[expression], center_x - 480, 60, 400, 400);
+}
+function wendyText(words){
+    fill("#3f6680");
+    rect(center_x-480, 450, 960, 150, 20);
+    fontStyle();
+    text(words, center_x - 440, 460, 920, 130);
+}
+    
+/**
+    Mouse navigation support.
+*/
+function mousePressed() {
+    console.log("mouse clicked.");
+}
+
+
+function preload() {
+    var i;
+    for (i = 0; i<9; i++){
+        wendy_sprite[i] = loadImage("https://acelaena.github.io/assets3/exp_"+ i + ".PNG");
+    }  
+}
+
+function setup() {
+    createCanvas(windowWidth-12, windowHeight-12);
+    center_x = width/2;
+    center_y = height/2; 
+    
+    textClicker = new Clickable(); 
+    textClicker.color = '#0000'; 
+    textClicker.text = '';
+    textClicker.strokeWeight = 0;  
+    textClicker.resize(960, 150);
+    textClicker.cornerRadius = 20;
+    textClicker.locate(center_x-480, 450);
+    textClicker.onPress = function(){  //When myButton is pressed
+        drawAgain = true;
+        console.log("clicky!");
+    } 
+}
+
+function draw() {
+    if (drawAgain){
+        fill("#ddd");
+        rect(center_x-480, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+        wendyExp(0);
+        wendyText("Hello world! My name is Wendy!");
+        drawAgain = false; 
+    }
+}
